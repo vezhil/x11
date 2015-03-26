ae0c2956efd2dfac771fe46606dddaf603afa276
vezhil <vezhil@hotmail.com>
2015-03-26 00:55:34 +0000

    Second commit

--- assignment_1.js
+++ assignment_1.js
@@ -1,32 +1,23 @@
 var b = require('bonescript');
 var fs = require('fs');
-var bit1 = "P8_7";
-var bit2 = "P8_9";
-var file1 = 'par1.inx';
-var file2 = 'par2.inx';
+var bit1 = "P8_9";
+var file1 = 'timerfile.inx';
 
 var storeFreq_secs = 15;
-var count1 = fs.readFileSync(file1,'utf8');
-var count2 = fs.readFileSync(file2,'utf8');
+var counter = fs.readFileSync(file1,'utf8');
 
 b.pinMode(bit1,b.INPUT,7);
-b.pinMode(bit2,b.INPUT,7);
 
-while(true){
-    if(b.digitalRead(bit1)==b.HIGH)
-        setInterval(counter(count1),1000);    
-    if(b.digitalRead(bit2)==b.HIGH)
-        setInterval(counter(count2),1000)
-}
+setInterval(count,1000);    
 
-
-function counter(i){
-    if(b.digitalRead(inbit)==b.HIGH) {
-        i++;
+function count(){
+    console.log('here1');
+    if(b.digitalRead(bit1)==b.HIGH) {
+        counter++
         console.log(" :"+counter);
     }
-    if(i%storeFreq_secs == 0)
-        fs.writeFile(fileio,counter,function(err){
+    if(counter%storeFreq_secs == 0)
+        fs.writeFile(file1,counter,function(err){
             if (err) throw err;
         });
 }
